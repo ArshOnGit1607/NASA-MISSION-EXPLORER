8a8a6686938c17d4c47826900c3dfa6131f4674a
Arshnoor Singh <arshnoor1607@gmail.com>
2026-03-31 12:00:00 +0530

    Added Header component

---
+++ NASA_PROJECT/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header(){
+    return(
+        <div className="Header">
+            <h1>NASA Mission Explorer</h1>
+            <p>Explore the universe with NASA</p>
+        </div>
+    )
+}
+export default Header
