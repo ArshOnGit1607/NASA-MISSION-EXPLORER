6235cbde41eddc6c9e2aacb2de03566ad3f3dba7
Arshnoor Singh <arshnoor1607@gmail.com>
2026-03-31 12:00:00 +0530

    Added Footer component

---
+++ NASA_PROJECT/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <div className="Footer">
+            <p>&copy; 2026 NASA Mission Explorer. All rights reserved.</p>
+        </div>
+    );
+}
+
+export default Footer;
